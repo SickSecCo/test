8ed3369c6a031db791493abc5a0047ea3eb23919
SickSecCo <deasy3067@gmail.com>
2022-11-02 18:49:45 +0100

    ok

--- microshell.c
+++ microshell.c
@@ -1,83 +1,92 @@
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include <string.h>
 
-int err(char *a, char *b)
+
+int put_err(char *str, char *err)
 {
-    while (*a)
-        write(2, a++, 1);
-    if (b)
-        while(*b)
-            write(2, b++, 1);
-    write(2, "\n", 1);
-    return (1);
+	while(*str)
+		write(2, str++, 1);
+	if (err)
+		while(*err)
+			write(2, err++, 1);
+	write(2, "\n", 1);
+	return 1;
 }
 
-int exec(char **argv, int i, int tmp, char **envp)
+void cd_builtin(char **argv, int i)
 {
-    argv[i] = NULL;
-    dup2(tmp, 0);
-    close(tmp);
-    execve(argv[0], argv, envp);
-    return (err("error: cannot execute ", argv[0]));
+	if (i != 2)
+		put_err("error: cd: bad arguments", NULL);
+	else if (chdir(argv[1]))
+		put_err("error: cd: cannot change directory to: ", argv[1]);
+}
+
+int exec(char **argv, char **envp, int i, int tmp)
+{
+	argv[i] = NULL;
+	dup2(tmp, 0);
+	close(tmp);
+	execve(argv[0], argv, envp);
+	return (put_err("error: cannot execute ", argv[0]));
+}
+
+int last_command(char **argv, char **envp, int i, int *tmp)
+{
+	int pid;
+	pid = fork();
+	if (!pid)
+		if (exec(argv, envp, i, *tmp))
+			return 1;
+	else
+	{
+		close(*tmp);
+		waitpid(pid, NULL, WUNTRACED);
+		*tmp = dup(0);
+	}
+	return 0;
+}
+
+int command_exec(char **argv, char **envp, int i, int *tmp)
+{
+	int pid;
+	int fd[2];
+	pipe(fd);
+	if ((pid = fork()) == 0)
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		if (exec(argv, envp, i, *tmp))
+			return 1;
+	}
+	else
+	{
+		close(tmp);
+		close(fd[1]);
+		*tmp = fd[0];
+	}
+	return 0;
 }
 
 int main(int argc, char **argv, char **envp)
 {
-    int i = 0;
-    int tmp = dup(0);
-    int pid = 0;
-    int fd[2];
-    (void)argc;
-    while (argv[i] && argv[i + 1])
-    {
-        argv = &argv[i + 1];
-        i = 0;
-        while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
-            i++;
-        if (!strcmp(argv[0], "cd"))
-        {
-            if (i != 2)
-                err("error: cd: bad arguments", NULL);
-            else if (chdir(argv[1]) != 0)
-                err("error: cd: cannot change directory to: ", argv[1]);
-        }
-        else if (i != 0 && (argv[i] == NULL || !strcmp(argv[i], ";")))
-        {
-            pid = fork();
-            if (!pid)
-            {
-                if (exec(argv, i, tmp, envp))
-                    return (1);
-            }
-            else
-            {
-                close(tmp);
-                while (waitpid(-1, NULL, WUNTRACED) != -1)
-                    ;
-                tmp = dup(0);
-            }
-        }
-        else if (i != 0 && !strcmp(argv[i], "|"))
-        {
-            pipe(fd);
-            pid = fork();
-            if (!pid)
-            {
-                dup2(fd[1], 1);
-                close(fd[0]);
-                close(fd[1]);
-                if (exec(argv, i, tmp, envp))
-                    return (1);
-            }
-            else
-            {
-                close(tmp);
-                close(fd[1]);
-                tmp = fd[0];
-            }
-        }
-    }
-    close(tmp);
-    return (0);
+	(void) argc;
+	int temp = dup(0);
+	int i = -1;
+	while (argv[i] && argv[i + 1])
+	{
+		argv = &argv[i + 2];
+		i = -1;
+		while (argv[++i] && strcmp(argv[i], "|") &&  strcmp(argv[i], ";"));
+		i += 1;
+		if (strcmp(argv[0], "cd"))
+			cd_builtin(argv, i);
+		else if (i != 0 && (!strcmp(argv[i], ";") || !argv[i]))
+			last_command(argv, envp, i, &temp);
+		else if (i != 0 && strcmp(argv[i], "|"))
+			command_exec(argv, envp, i, &temp);
+	}
+	close(temp);
+	return 0;
 }
